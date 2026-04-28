86fac8e09ff8682b0b479a9e9136bb064faea2c9
MukeshKumarQA <Mukusharma1996@gmail.com>
2026-04-28 21:16:54 +0530

    Fix Issues for Variables

--- terraform/modules/ecr/variables.tf
+++ terraform/modules/ecr/variables.tf
@@ -1,1 +1,11 @@
 # ECR module input variables
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "tags" {
+  description = "Common resource tags"
+  type        = map(string)
+  default     = {}
+}
