6dae46b3b05dcffe3e7566988ad382ae3515e7d3
MukeshKumarQA <Mukusharma1996@gmail.com>
2026-04-28 21:13:35 +0530

    Added Code for Terraform EC

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -1,1 +1,9 @@
-# ECR module outputs
+output "repository_url" {
+  description = "ECR repository URL"
+  value       = aws_ecr_repository.this.repository_url
+}
+
+output "repository_name" {
+  description = "ECR repository name"
+  value       = aws_ecr_repository.this.name
+}# ECR module outputs
