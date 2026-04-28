db44f059bee9eef52d74c06826311968cdabad0f
MukeshKumarQA <Mukusharma1996@gmail.com>
2026-04-28 19:51:21 +0530

    Implemented modular IAM roles for EKS cluster and worker nodes

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -109,4 +109,4 @@ resource "aws_route_table_association" "private" {
 
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
-}# VPC module resources
+} # VPC module resources
