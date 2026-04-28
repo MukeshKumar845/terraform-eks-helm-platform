79c898b7a6e6850486b5e3d3ad1ff694185f596d
MukeshKumarQA <Mukusharma1996@gmail.com>
2026-04-28 21:15:40 +0530

    Added variables and outputs

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,1 +1,39 @@
 # ECR module resources
+resource "aws_ecr_repository" "this" {
+  name                 = "${var.project_name}-app-repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = merge(var.tags, {
+    Name = "${var.project_name}-ecr"
+  })
+}
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  repository = aws_ecr_repository.this.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Expire untagged images after 30 days"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 30
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
